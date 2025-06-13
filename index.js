401405cb368a79b25294fa8726cb56ef78bda4c0
Jamie <jamieyw.hu@gmail.com>
2025-06-13 04:37:15 -0700

    update kambaz

--- index.js
+++ index.js
@@ -2,12 +2,36 @@ import express from 'express';
 import Hello from "./Hello.js";
 import Lab5 from './Lab5/index.js';
 import cors from "cors";
+import UserRoutes from './Kambaz/Users/routes.js';
+import "dotenv/config";
+import session from "express-session";
 
 const app = express();
-app.use(cors());  // make sure cors is used right after creating the app express instance
+// make sure cors is used right after creating the app express instance
+app.use(cors({
+   credentials: true,
+   origin: process.env.NETLIFY_URL || "http://localhost:5173",
+ }
+));
+// configure server sessions after cors
+const sessionOptions = {
+  secret: process.env.SESSION_SECRET || "kambaz",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.NODE_SERVER_DOMAIN,
+  };
+}
+app.use(session(sessionOptions));
 app.use(express.json());  // make sure this statement occurs AFTER setting up CORS but BEFORE all the routes
 Hello(app);
 Lab5(app);
+UserRoutes(app);
 
 // Use the PORT environment variable if it's set (e.g., in production),
 // otherwise, default to a local port (e.g., 4000) for development.
